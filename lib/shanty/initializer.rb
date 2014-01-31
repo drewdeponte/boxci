ae7edfe2cb3d02b0957ad5387459d7f022421125
Russell Cloak <russell.cloak@reachlocal.com>
2014-01-30 17:05:44 -0800

    Make the initializer a Thor class
    It now works like the rest of the class.

--- lib/shanty/initializer.rb
+++ lib/shanty/initializer.rb
@@ -1,8 +1,24 @@
+require "thor"
+
 module Shanty
-  class Initializer
+  class Initializer < Thor
+    include Thor::Actions
+
     class << self
-      def init
-        Shanty::DependencyChecker.verify_all
+      def source_root
+        File.dirname(__FILE__)
+      end
+    end
+
+    desc "", "" #TODO: Figure out how to remove
+    def init
+      Shanty::DependencyChecker.verify_all
+      create_base_files
+    end
+
+    no_commands do
+      def create_base_files
+        directory "templates/.shanty", ".shanty"
       end
     end
   end
--- spec/lib/shanty/initializer_spec.rb
+++ spec/lib/shanty/initializer_spec.rb
@@ -1,11 +1,19 @@
 require "spec_helper"
 
 describe Shanty::Initializer do
-  subject { Shanty::Initializer }
-
   describe ".init" do
+    before do
+      allow(Shanty::DependencyChecker).to receive(:verify_all)
+      allow(Shanty::DependencyChecker).to receive(:create_base_files)
+    end
+
     it "verifies all required dependencies are installed" do
       expect(Shanty::DependencyChecker).to receive(:verify_all)
+      subject.init
+    end
+
+    it "creates the base shanty files" do
+      expect(subject).to receive(:create_base_files)
       subject.init
     end
   end
