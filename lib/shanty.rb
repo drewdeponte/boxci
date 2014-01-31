6df2baea0edd120bb1da90b25263b3687a2195a7
Russell Cloak <russell.cloak@reachlocal.com>
2014-01-30 17:04:02 -0800

    Setup "generate" as a Thor subcommand
    This allows commands to be chained down from the bin to the generator
    class. For instance: shanty generator [template]

--- lib/shanty.rb
+++ lib/shanty.rb
@@ -2,6 +2,7 @@ require "shanty/version"
 require "shanty/vm_builder"
 require "shanty/initializer"
 require "shanty/dependency_checker"
+require "shanty/generator"
 
 module Shanty
   class MissingDependency < StandardError; end
