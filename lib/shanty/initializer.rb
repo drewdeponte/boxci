c2252a52ad40bcaabb061102c00541cc5854d89d
Russell Cloak <russell.cloak@reachlocal.com>
2014-01-31 11:22:24 -0800

    Use the actual file names for init sample files

--- lib/shanty/initializer.rb
+++ lib/shanty/initializer.rb
@@ -2,6 +2,8 @@ require "thor"
 
 module Shanty
   class Initializer < Thor
+    attr_accessor :local_repository_path
+
     include Thor::Actions
 
     class << self
@@ -13,12 +15,19 @@ module Shanty
     desc "", "" #TODO: Figure out how to remove
     def init
       Shanty::DependencyChecker.verify_all
-      create_base_files
+      @local_repository_path = %x(pwd)
+      create_home_directory_files
+      create_repo_root_files
     end
 
     no_commands do
-      def create_base_files
-        directory "templates/.shanty", ".shanty"
+      def create_home_directory_files
+        directory "templates/shanty", "~/.shanty"
+      end
+
+      def create_repo_root_files
+        copy_file "templates/Vagrantfile", @local_repository_path
+        directory "templates/puppet", File.join(@local_repository_path.strip, "puppet")
       end
     end
   end
--- spec/lib/shanty/initializer_spec.rb
+++ spec/lib/shanty/initializer_spec.rb
@@ -1,10 +1,11 @@
 require "spec_helper"
 
 describe Shanty::Initializer do
-  describe ".init" do
+  describe "#init" do
     before do
       allow(Shanty::DependencyChecker).to receive(:verify_all)
-      allow(Shanty::DependencyChecker).to receive(:create_base_files)
+      allow(subject).to receive(:create_home_directory_files)
+      allow(subject).to receive(:create_repo_root_files)
     end
 
     it "verifies all required dependencies are installed" do
@@ -12,9 +13,41 @@ describe Shanty::Initializer do
       subject.init
     end
 
-    it "creates the base shanty files" do
-      expect(subject).to receive(:create_base_files)
+    it "creates the home directory shanty files" do
+      expect(subject).to receive(:create_home_directory_files)
       subject.init
+    end
+
+    it "creates the repo root shanty files" do
+      expect(subject).to receive(:create_repo_root_files)
+      subject.init
+    end
+  end
+
+  describe "#create_home_directory_files" do
+    it "copies the shanty directory from the templates, to the user's root" do
+      expect(subject).to receive(:directory).with("templates/shanty", "~/.shanty")
+      subject.create_home_directory_files
+    end
+  end
+
+  describe "#create_repo_root_files" do
+    let(:local_repository_path) { "/somewhere" }
+
+    before do
+      subject.instance_variable_set(:@local_repository_path, local_repository_path)
+      allow(subject).to receive(:copy_file)
+      allow(subject).to receive(:directory)
+    end
+
+    it "copies the Vagrantfile from the templates to the project root" do
+      expect(subject).to receive(:copy_file).with("templates/Vagrantfile", local_repository_path)
+      subject.create_repo_root_files
+    end
+
+    it "copies the puppet directory from the templates to the project root" do
+      expect(subject).to receive(:directory).with("templates/puppet", "#{local_repository_path}/puppet")
+      subject.create_repo_root_files
     end
   end
 end
