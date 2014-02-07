bd909fad023707969015a5b500601b2ff25744d0
Russell Cloak <russell.cloak@reachlocal.com>
2014-02-06 16:54:19 -0800

    Clean up the dependency checker

--- lib/shanty/dependency_checker.rb
+++ lib/shanty/dependency_checker.rb
@@ -1,14 +1,12 @@
 module Shanty
-  class DependencyChecker
-    attr_accessor :local_repository_path
-
-    class << self
+  class DependencyChecker < Shanty::Base
+    no_commands do
       def verify_all
-        @local_repository_path = File.expand_path(%x(pwd)).strip
         begin
           self.verify_vagrant
           self.verify_cloud_provider_config
-          self.verify_repo_puppet_files
+          self.verify_repo_puppet_directory
+          self.verify_vagrantfile
           # List other dependencies here
         rescue Shanty::MissingDependency => e
           puts e.message
@@ -28,14 +26,26 @@ module Shanty
         end
       end
 
-      def verify_repo_puppet_files
-        vagrant_file = File.join(@local_repository_path, "Vagrantfile")
-        puppet_directory = File.join(@local_repository_path, "puppet")
-        manifest_file = File.join(@local_repository_path, "puppet", "manifests", "main.pp")
-        error_message = "It looks like you don't have a Vagrant/Puppet setup for your repository. You can generate example files by running: \"shanty init\""
+      def verify_repo_puppet_directory
+        puppet_directory = File.join(local_repository_path, "puppet")
+        error_message = "It looks like you don't have Puppet files setup for your repository. You can generate example files by running: \"shanty init\""
 
-        if !File.exists?(vagrant_file) || !File.directory?(puppet_directory) || !File.exists?(manifest_file)
+        if !File.directory?(puppet_directory)
           raise Shanty::MissingDependency, error_message
+        end
+      end
+
+      def verify_vagrantfile
+        vagrant_file = File.join(local_repository_path, "Vagrantfile")
+        if !File.exists?(vagrant_file)
+          raise Shanty::MissingDependency, "It looks like you don't have a Vagrantfile setup for your repository. You can generate an example file by running: \"shanty generate vagrantfile\" "
+        end
+      end
+
+      def verify_shanty_config
+        config_file = File.join(local_repository_path, "shanty.yml")
+        if !File.exists?(config_file)
+          raise Shanty::MissingDependency, "It looks like you're missing the Shanty configuration file. You can generate an example file by running: \"shanty init\" "
         end
       end
     end
--- spec/lib/shanty/dependency_checker_spec.rb
+++ spec/lib/shanty/dependency_checker_spec.rb
@@ -1,18 +1,12 @@
 require "spec_helper"
 
 describe Shanty::DependencyChecker do
-  subject { Shanty::DependencyChecker }
-
-  describe ".verify_all" do
+  describe "#verify_all" do
     before do
       allow(subject).to receive(:verify_vagrant)
       allow(subject).to receive(:verify_cloud_provider_config)
-      allow(subject).to receive(:verify_repo_puppet_files)
-    end
-
-    it "gets the local repository path" do
-      expect(File).to receive(:expand_path).and_return("path")
-      subject.verify_all
+      allow(subject).to receive(:verify_repo_puppet_directory)
+      allow(subject).to receive(:verify_vagrantfile)
     end
 
     it "checks if vagrant is installed" do
@@ -25,8 +19,13 @@ describe Shanty::DependencyChecker do
       subject.verify_all
     end
 
-    it "verifies the repository has the required puppet files" do
-      expect(subject).to receive(:verify_repo_puppet_files)
+    it "verifies the repository has the required puppet directory structure" do
+      expect(subject).to receive(:verify_repo_puppet_directory)
+      subject.verify_all
+    end
+
+    it "verifies the vagrantfile exists" do
+      expect(subject).to receive(:verify_vagrantfile)
       subject.verify_all
     end
 
@@ -69,7 +68,7 @@ describe Shanty::DependencyChecker do
     end
   end
 
-  describe ".verify_vagrant" do
+  describe "#verify_vagrant" do
     it "checks if the system path includes 'vagrant'" do
       expect(subject).to receive(:system).with("which vagrant > /dev/null").and_return(true)
       subject.verify_vagrant
@@ -96,7 +95,7 @@ describe Shanty::DependencyChecker do
     end
   end
 
-  describe ".verify_cloud_provider_config" do
+  describe "#verify_cloud_provider_config" do
     it "checks if the the '.cloud_provider_config' exists in the user's home dir" do
       cloud_provider_file = File.join(File.expand_path(ENV["HOME"]), ".shanty", "cloud_provider_config.yml")
       expect(File).to receive(:exists?).with(cloud_provider_file).and_return(true)
@@ -124,43 +123,92 @@ describe Shanty::DependencyChecker do
     end
   end
 
-  describe ".verify_repo_puppet_files" do
+  describe "#verify_repo_puppet_directory" do
     let(:local_repository_path) { "/some/path" }
 
     before do
-      subject.instance_variable_set(:@local_repository_path, local_repository_path)
-      allow(File).to receive(:exists?).and_return(true)
+      allow(subject).to receive(:local_repository_path).and_return(local_repository_path)
       allow(File).to receive(:directory?).and_return(true)
+    end
+
+    it "checks for the puppet directory in the repo root" do
+      expect(File).to receive(:directory?).with(File.join(local_repository_path, "puppet")).and_return(true)
+      subject.verify_repo_puppet_directory
+    end
+
+    context "when the directory is present" do
+      it "does not raise an error" do
+        expect { subject.verify_repo_puppet_directory }.to_not raise_error
+      end
+    end
+
+    context "when the directory is not present" do
+      before do
+        allow(File).to receive(:directory?).and_return(false)
+      end
+
+      it "raises an error" do
+        expect { subject.verify_repo_puppet_directory }.to raise_error(Shanty::MissingDependency)
+      end
+    end
+  end
+
+  describe "#verify_vagrantfile" do
+    let(:local_repository_path) { "/some/path" }
+
+    before do
+      allow(subject).to receive(:local_repository_path).and_return(local_repository_path)
+      allow(File).to receive(:exists?).and_return(true)
     end
 
     it "checks for the Vagrantfile in the repo root" do
       expect(File).to receive(:exists?).with(File.join(local_repository_path, "Vagrantfile")).and_return(true)
-      subject.verify_repo_puppet_files
+      subject.verify_vagrantfile
     end
 
-    it "checks for the puppet in the repo root" do
-      expect(File).to receive(:directory?).with(File.join(local_repository_path, "puppet")).and_return(true)
-      subject.verify_repo_puppet_files
-    end
-
-    it "checks for the puppet manifest in the puppet directory" do
-      expect(File).to receive(:exists?).with(File.join(local_repository_path, "puppet", "manifests", "main.pp")).and_return(true)
-      subject.verify_repo_puppet_files
-    end
-
-    context "when the files are all verified" do
+    context "when the vagrantfile is verified" do
       it "does not raise an error" do
-        expect { subject.verify_repo_puppet_files }.to_not raise_error
+        expect { subject.verify_vagrantfile }.to_not raise_error
       end
     end
 
-    context "when at least one of the files is not verified" do
+    context "when the vagrantfile is not verified" do
       before do
         allow(File).to receive(:exists?).and_return(false)
       end
 
       it "raises an error" do
-        expect { subject.verify_repo_puppet_files }.to raise_error(Shanty::MissingDependency)
+        expect { subject.verify_vagrantfile }.to raise_error(Shanty::MissingDependency)
+      end
+    end
+  end
+
+  describe "#verify_shanty_config" do
+    let(:local_repository_path) { "/some/path" }
+
+    before do
+      allow(subject).to receive(:local_repository_path).and_return(local_repository_path)
+      allow(File).to receive(:exists?).and_return(true)
+    end
+
+    it "checks for the Shanty config file in the repo root" do
+      expect(File).to receive(:exists?).with(File.join(local_repository_path, "shanty.yml")).and_return(true)
+      subject.verify_shanty_config
+    end
+
+    context "when the config file is verified" do
+      it "does not raise an error" do
+        expect { subject.verify_shanty_config }.to_not raise_error
+      end
+    end
+
+    context "when the config file is not verified" do
+      before do
+        allow(File).to receive(:exists?).and_return(false)
+      end
+
+      it "raises an error" do
+        expect { subject.verify_shanty_config }.to raise_error(Shanty::MissingDependency)
       end
     end
   end
