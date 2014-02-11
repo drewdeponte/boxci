008ae420581c9ca06b5ec952f653b4d6e6a9f6a2
Russell Cloak <russell.cloak@reachlocal.com>
2014-02-11 14:20:51 -0800

    Move provider specific config values into their respective classes

--- lib/shanty/providers/aws.rb
+++ lib/shanty/providers/aws.rb
@@ -1,6 +1,10 @@
 module Shanty
   module Providers
     class Aws < ::Shanty::Provider
+      PLUGIN = {
+        plugin: "vagrant-aws",
+        dummy_box_url: "https://github.com/mitchellh/vagrant-aws/raw/master/dummy.box"
+      }
 
       no_commands do
         def generate_provider_vagrantfile
