6a0549b76c45e5a314422760fa595267003b100c
Andrew De Ponte <cyphactor@gmail.com>
2014-02-11 22:18:06 -0800

    Add project config tests for script hooks.

--- spec/lib/shanty/project_config_spec.rb
+++ spec/lib/shanty/project_config_spec.rb
@@ -40,24 +40,68 @@ describe Shanty::ProjectConfig do
     end
   end
 
-  describe "#hook_as_array" do
-    context "when the value is an array" do
-      it "returns the value"
-    end
-
-    context "when not the value is not an array" do
-      it "returns the value as the only item in an array"
+  describe "#before_install" do
+    it "grabs the named hook as an array" do
+      expect(subject).to receive(:hook_as_array).with('before_install')
+      subject.before_install
     end
   end
 
-  it "add all the tests for the script hooks!" do
-    pending
-    # before_install
-    # install
-    # before_script
-    # script
-    # after_failure
-    # after_success
-    # after_script
+  describe "#install" do
+    it "grabs the named hook as an array" do
+      expect(subject).to receive(:hook_as_array).with('install')
+      subject.install
+    end
+  end
+
+  describe "#before_script" do
+    it "grabs the named hook as an array" do
+      expect(subject).to receive(:hook_as_array).with('before_script')
+      subject.before_script
+    end
+  end
+
+  describe "#script" do
+    it "grabs the named hook as an array" do
+      expect(subject).to receive(:hook_as_array).with('script')
+      subject.script
+    end
+  end
+
+  describe "#after_failure" do
+    it "grabs the named hook as an array" do
+      expect(subject).to receive(:hook_as_array).with('after_failure')
+      subject.after_failure
+    end
+  end
+
+  describe "#after_success" do
+    it "grabs the named hook as an array" do
+      expect(subject).to receive(:hook_as_array).with('after_success')
+      subject.after_success
+    end
+  end
+
+  describe "#after_script" do
+    it "grabs the named hook as an array" do
+      expect(subject).to receive(:hook_as_array).with('after_script')
+      subject.after_script
+    end
+  end
+
+  describe "#hook_as_array" do
+    context "when the value is an array" do
+      it "returns the value" do
+        subject.instance_variable_set(:@project_config, { 'foo' => [1, 2, 3] })
+        expect(subject.send(:hook_as_array, 'foo')).to eq([1, 2, 3])
+      end
+    end
+
+    context "when not the value is not an array" do
+      it "returns the value as the only item in an array" do
+        subject.instance_variable_set(:@project_config, { 'foo' => 1 })
+        expect(subject.send(:hook_as_array, 'foo')).to eq([1])
+      end
+    end
   end
 end
