aadd4155cd6c58af32844697a0d9659ae8163236
Andrew De Ponte <cyphactor@gmail.com>
2014-02-14 00:40:29 -0800

    Add new language hooks to the Ruby language.
    I did this so that I could start using the new language hooks in the test
    runner generation.

--- lib/shanty/languages/ruby.rb
+++ lib/shanty/languages/ruby.rb
@@ -3,6 +3,18 @@ module Shanty
     class Ruby < ::Shanty::Language
 
       no_commands do
+        def default_script
+          %q{bundle exec rake}
+        end
+
+        def before_permutation_switch
+          %q{export RAILS_ENV=test}
+        end
+
+        def after_permutation_switch
+          %q{gem install bundler && bundle install --without assets production}
+        end
+
         def generate_starter_puppet_manifest
           @project_config = Shanty.project_config
           if @project_config.rbenv
