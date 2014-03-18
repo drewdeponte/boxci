5c575fdac39e4c589107e0d46f7e27680827dadf
Andrew De Ponte <cyphactor@gmail.com>
2014-03-17 23:58:35 -0700

    Made Builder & Tester no longer inherit from Thor.
    I made this change because they aren't actually classes that provide
    subcomands. Instead they simply need the Thor::Actions so that I can use that
    various functionality. I also fixed the redirecting in the cleanup step
    commands so that it is correct.

--- lib/shanty/builder.rb
+++ lib/shanty/builder.rb
@@ -1,30 +1,29 @@
 require "thor"
 
 module Shanty
-  class Builder < Thor
+  class Builder
+    include Thor::Base
     include Thor::Actions
 
     source_root(File.dirname(__FILE__))
 
-    no_commands do
-      def build
-        generate_project_vagrantfile
-        generate_starter_puppet_manifest
-      end
+    def build
+      generate_project_vagrantfile
+      generate_starter_puppet_manifest
+    end
 
-      def generate_project_vagrantfile
-        dependency_checker = Shanty::DependencyChecker.new
-        dependency_checker.verify_shanty_config
+    def generate_project_vagrantfile
+      dependency_checker = Shanty::DependencyChecker.new
+      dependency_checker.verify_shanty_config
 
-        @project_config = Shanty.project_config
-        template "templates/Vagrantfile", File.join(Shanty.project_path, "Vagrantfile")
-      end
+      @project_config = Shanty.project_config
+      template "templates/Vagrantfile", File.join(Shanty.project_path, "Vagrantfile")
+    end
 
-      def generate_starter_puppet_manifest
-        directory "templates/puppet", File.join(Shanty.project_path, "puppet")
-        language = Shanty::LanguageFactory.build(Shanty.project_config.language)
-        language.generate_starter_puppet_manifest
-      end
+    def generate_starter_puppet_manifest
+      directory "templates/puppet", File.join(Shanty.project_path, "puppet")
+      language = Shanty::LanguageFactory.build(Shanty.project_config.language)
+      language.generate_starter_puppet_manifest
     end
   end
 end
