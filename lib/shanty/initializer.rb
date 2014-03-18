cd1bda8e0961743c426c0bb5b25e4378735ac1eb
Andrew De Ponte <cyphactor@gmail.com>
2014-03-17 22:57:39 -0700

    Make Initializer not a inherit from Thor.
    I made this change to have it no longer inherit from Thor because it isn't
    really a sub command. Instead it is simply a class that I wanted to use the
    thor action methods in.

--- lib/shanty/initializer.rb
+++ lib/shanty/initializer.rb
@@ -1,42 +1,40 @@
 require "thor"
 
 module Shanty
-  class Initializer < Thor
+  class Initializer
+    include Thor::Base
     include Thor::Actions
-
     source_root(File.dirname(__FILE__))
 
-    no_commands do
-      def init(language, provider)
-        create_provider_config(provider)
-        set_default_provider(provider)
-        create_project_config(language)
-      end
+    def init(language, provider)
+      create_provider_config(provider)
+      set_default_provider(provider)
+      create_project_config(language)
+    end
 
-      def create_provider_config(provider)
-        template "templates/providers/#{provider}.yml.tt", "~/.shanty/providers/#{provider}.yml"
-      end
+    def create_provider_config(provider)
+      template "templates/providers/#{provider}.yml.tt", "~/.shanty/providers/#{provider}.yml"
+    end
 
-      # TODO: Change this name to create_global_config
-      def set_default_provider(provider)
-        @provider = provider
-        template "templates/shanty/global_config.yml.tt", "~/.shanty/global_config.yml"
-      end
+    # TODO: Change this name to create_global_config
+    def set_default_provider(provider)
+      @provider = provider
+      template "templates/shanty/global_config.yml.tt", "~/.shanty/global_config.yml"
+    end
 
-      def create_project_config(language)
-        shanty_file = File.join(Shanty.project_path, ".shanty.yml")
-        @language = language
-        @current_ruby_version = dot_ruby_version
-        template "templates/dot_shanty.yml.tt", shanty_file
-      end
+    def create_project_config(language)
+      shanty_file = File.join(Shanty.project_path, ".shanty.yml")
+      @language = language
+      @current_ruby_version = dot_ruby_version
+      template "templates/dot_shanty.yml.tt", shanty_file
+    end
 
-      def dot_ruby_version
-        dot_ruby_version_file_path = File.join(Shanty.project_path, ".ruby-version")
-        return nil unless File.exist?(dot_ruby_version_file_path)
-        dot_ruby_version_content = File.read(dot_ruby_version_file_path).strip
-        if dot_ruby_version_content =~ /(?:ruby-)?(\d+\.\d+\.\d+(?:-p\d+)?)(?:@[\w\-]+)?/
-          return $1
-        end
+    def dot_ruby_version
+      dot_ruby_version_file_path = File.join(Shanty.project_path, ".ruby-version")
+      return nil unless File.exist?(dot_ruby_version_file_path)
+      dot_ruby_version_content = File.read(dot_ruby_version_file_path).strip
+      if dot_ruby_version_content =~ /(?:ruby-)?(\d+\.\d+\.\d+(?:-p\d+)?)(?:@[\w\-]+)?/
+        return $1
       end
     end
   end
